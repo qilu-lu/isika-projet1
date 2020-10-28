3dc74b1e64e2373939122b467b5e08f989d237b3
qilu-lu <celestineqi0@gmail.com>
2020-10-28 22:49:27 +0100

    maven + gitIgnore

--- AnnuaireInformatiseFX/src/main/java/application/Main.java
+++ AnnuaireInformatiseFX/src/main/java/application/Main.java
@@ -1,9 +1,9 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
@@ -12,7 +12,7 @@ public class Main extends Application {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getClassLoader().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
