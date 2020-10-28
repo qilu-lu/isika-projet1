d2577a0fd72ec7dc544396f0cc6a53b2b042cc6c
Roman Kulbacki <roman.kulbacki@gmail.com>
2020-10-28 18:29:10 +0100

    Stagiaire début

--- AnnuaireInformatise/src/main/java/fr/isika/cdi7/model/Stagiaire.java
+++ AnnuaireInformatise/src/main/java/fr/isika/cdi7/model/Stagiaire.java
@@ -4,5 +4,88 @@ public class Stagiaire {
 
 	private String nom;
 	private String prenom;
+	private String departement;
+	private	String promotion;
+	private int annee;
 	
+	
+	
+	
+	public Stagiaire() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Stagiaire(String nom, String prenom, String departement, String promotion, int annee) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.departement = departement;
+		this.promotion = promotion;
+		this.annee = annee;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	@Override
+	public String toString() {
+		return "Stagiaire [nom=" + nom + ", prenom=" + prenom + ", promotion=" + promotion + ", annee=" + annee + "]";
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getPromotion() {
+		return promotion;
+	}
+	public void setPromotion(String promotion) {
+		this.promotion = promotion;
+	}
+
+	public int getAnnee() {
+		return annee;
+	}
+	public void setAnnee(int annee) {
+		this.annee = annee;
+	}
+
+	public String getDepartement() {
+		return departement;
+	}
+	public void setDepartement(String departement) {
+		this.departement = departement;
+	}
 }
