a3ea878942641633aa6a717b10ab375bdee56360
qilu-lu <celestineqi0@gmail.com>
2020-10-29 11:25:18 +0100

    ajout arbre

--- AnnuaireInformatiseFX/src/main/java/application/Main.java
+++ AnnuaireInformatiseFX/src/main/java/application/Main.java
@@ -1,26 +1,45 @@
 package application;
-	
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root, 400, 400);
 			scene.getStylesheets().add(getClass().getClassLoader().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws IOException {
+		File file=new File("qilu/desktop/STAGIAIRES.DON");
+		
+		FileReader reader =new FileReader(file);
+		BufferedReader br=new BufferedReader(reader);
+		// LineNumberReader reader = new LineNumberReader(in); 
+		br.read();
+		
+		
+		
+		
+		
 		launch(args);
+
+		ArbreStagiaire1<Stagiaire> s = new ArbreStagiaire1<>();
+		s.ajouterNoeud(new Stagiaire());
 	}
 }
--- AnnuaireInformatiseFX/src/main/java/application/Stagiaire.java
+++ AnnuaireInformatiseFX/src/main/java/application/Stagiaire.java
@@ -1,5 +1,62 @@
 package application;
 
-public class Stagiaire {
+public class Stagiaire implements Comparable<Stagiaire> {
+	private String nom;
+	private String prenom;
+	private String appartement;
+	private String promotion;
+	private int annee;
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getAppartement() {
+		return appartement;
+	}
+
+	public void setAppartement(String appartement) {
+		this.appartement = appartement;
+	}
+
+	public String getPromotion() {
+		return promotion;
+	}
+
+	public void setPromotion(String promotion) {
+		this.promotion = promotion;
+	}
+
+	public int getAnnee() {
+		return annee;
+	}
+
+	public void setAnnee(int annee) {
+		this.annee = annee;
+	}
+
+	@Override
+	public int compareTo(Stagiaire o) {
+	int result=this.getNom().compareTo(o.getNom());
+	if(result>0) {
+		
+	}
+		
+		
+		
+		return 0;
+	}
 
 }
